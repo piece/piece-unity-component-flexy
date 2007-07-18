5cdfe4f92f3ec578dac432efaf3877ddb0384401
KUBO Atsuhiro <kubo@iteman.jp>
2007-07-18 10:08:52 +0000

    - A minor improvement.

--- tests/Piece/Unity/Service/FlexyElementTestCase.php
+++ tests/Piece/Unity/Service/FlexyElementTestCase.php
@@ -36,7 +36,7 @@
  * @since      File available since Release 1.0.0
  */
 
-require dirname(__FILE__) . '/../../../prepare.php';
+require_once realpath(dirname(__FILE__) . '/../../../prepare.php');
 require_once 'PHPUnit.php';
 require_once 'Piece/Unity/Service/FlexyElement.php';
 require_once 'Piece/Unity/Context.php';
